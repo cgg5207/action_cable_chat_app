a85635853b2997ebdcd1bbcf662428f875ff39b9
Grayson Chen <cgg5207@sina.com>
2016-09-19 00:47:16 +0800

    deploy heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,8 +36,8 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  config.action_cable.url = 'wss://peaceful-basin-31856.herokuapp.com/cable'
-  config.action_cable.allowed_request_origins = [ 'https://peaceful-basin-31856.herokuapp.com' ]
+  config.action_cable.url = 'wss://grayson-chat.herokuapp.com//cable'
+  config.action_cable.allowed_request_origins = [ 'https://grayson-chat.herokuapp.com' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
